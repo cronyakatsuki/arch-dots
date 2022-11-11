e31e70c2d6f58eee1efdd921720770dbcbdf8076
cronyakatsuki <64900606+cronyakatsuki@users.noreply.github.com>
2022-11-11 18:43:00 +0100

    Disable unneded services.

--- .config/dk/autorun.sh
+++ .config/dk/autorun.sh
@@ -19,8 +19,8 @@ start-program syncthing
 start-program dunst
 
 # just some stuppid stuff I hate my self for actually using
-start-program discord --start-minimized
-start-program ferdium
+# start-program discord --start-minimized
+# start-program ferdium
 
 # my password manager
 start-program keepassxc
